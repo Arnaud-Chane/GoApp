7f4c383e6c88a56b067c147b25dcd48c11833ed0
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2021-11-27 10:45:27 +0100

    First commit

---
+++ test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+  "main"
+  "math/rand"
+)
+
+func main(){
+  
+  var dice1 = rand.Intn(6)+1
+  var dice2 = rand.Intn(6)+1
+
+  fmt.Println("First dice is " + dice1 + " and the second is " + dice2 + ".")
+
+}
