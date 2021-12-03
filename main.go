38469bde98f60f80a4e85c27538f8e13a48e0f97
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2021-12-03 15:28:56 +0100

    Create a main file and a test file

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+  "fmt"
+  "math/rand"
+)
+
+func main(){
+  
+  var dice1 = rand.Intn(6)+1
+  var dice2 = rand.Intn(6)+1
+
+  fmt.Println("First dice is",dice1,"and the second is",dice2,".")
+
+  var name string
+  fmt.Print("You are ")
+  fmt.Scanln(&name)
+
+}
