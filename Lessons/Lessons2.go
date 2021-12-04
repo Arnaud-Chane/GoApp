8e0be8a8ea2841faa8936b67a4db14ef62d7f20c
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2021-12-04 15:36:21 +0100

    Add lessons for later.

---
+++ Lessons/Lessons2.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+  "fmt"
+)
+func main(){
+  fmt.Print("My weight on the surface of Mars is ")
+  fmt.Print(149.0 * 0.3783)
+  fmt.Print(" lbs, and I would be ")
+  fmt.Print(41 * 365 / 687)
+  fmt.Print(" year old.")
+}
+// fmt.Printf("My weihgt on the surface of %v is %v lbs./n, "Earth", 149.0)
+
