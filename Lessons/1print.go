098b187997d581395aba568d16d92392289e4ac7
Arnaud-Chane <59416180+Arnaud-Chane@users.noreply.github.com>
2021-12-10 12:02:37 +0100

    Add files via upload

---
+++ Lessons/1print.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main () {
+
+  fmt.Println("Hello World.")
+  fmt.Printf("My weight on the surface of Mars is %v lbs,", 149.0*0.3783) fmt.Printf(" and I would be %v years old.\n", 41*365/687)
+}
